8e360f2a1f8213a2d339d438f6d81ade0ac8c726
songfeng80 <alecwang80@hotmail.com>
2014-05-11 13:45:38 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,17 @@
+
+setwd("D:\\DS\\exploratory data analysis")
+
+#######readin the data and covernt the data and times variables
+powerdata<-read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?", colClasses = c("character", "character" ,rep("numeric", 7)))
+powerdata$Time<-strptime(paste(powerdata$Date,powerdata$Time), "%d/%m/%Y %H:%M:%S")
+powerdata$Date<-as.Date(powerdata$Date,format = "%d/%m/%Y")
+
+head(powerdata)
+
+#######subset data to those in the time frames
+subdata<-subset(powerdata, Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"))
+
+###############plot 2#############
+png("plot2.png",width = 480, height = 480, units = "px")
+plot(subdata$Time, subdata$Global_active_power, type='l',xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
